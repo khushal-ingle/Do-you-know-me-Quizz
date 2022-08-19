c01305567aaa4cdeb6cafe7db7c4a31ad6a448c1
khushal ingle <khushalingle151@gmail.com>
2022-08-19 07:26:23 +0530

    initial:commit

---
+++ index.js
@@ -0,0 +1,57 @@
+
+var readlineSync = require('readline-sync');
+var chalk = require('chalk');
+
+var score=0;
+var welcomeMessage = readlineSync.question("What is your Name? ");
+console.log(chalk.black.bgWhite("Welcome " + welcomeMessage + "! To Do You Know Me Quiz"));
+
+function play(question, answer) {
+  var userAnswer = readlineSync.question(chalk.black.bgBlue.bold(question));
+  if (userAnswer === answer) {
+    console.log(chalk.green("Yeahhhh! It's a right answer \n"));
+    score=score + 1;
+    
+   
+  } else {
+    
+    console.log(chalk.red("OOPSS! It's a wrong answer \n"));
+   // score=score - 1;
+    
+  }
+  console.log(" Your Current Score : ", score);
+   console.log("***************************** \n");
+}
+
+var questions = [
+  {
+    question: "Q1.) where do I live?",
+    answer: "in ritu's heart"
+  },
+  {
+    question: "Q2.) what is my favourite color? ",
+    answer: "black"
+  },
+  {
+    question: "Q3.) My favourite outdoor game? ",
+    answer: "football"
+  }, {
+    question: "Q4.) what is my favourite movie? ",
+    answer: "phir hera pheri"
+  },
+  {
+    question: "Q5.) What is the name of my favourite car? ",
+    answer: "Range rover"
+  }
+]
+
+
+// loop
+for (var i = 0; i < questions.length; i++) {
+  var currentQuestion = questions[i];
+  play(currentQuestion.question, currentQuestion.answer);
+}
+
+console.log("Yay! You HAVE SCORED 5 OUT OF / "+ score)
+
+//​embed=1&output=1
